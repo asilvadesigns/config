4a2908379b962846fb8b4048ef35e446568b07cb
Alberto Silva <albertos@nvidia.com>
2022-10-19 13:21:55 -0400

    feat(workspace): update language servers

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -13,7 +13,6 @@ local options = {
     -- Enable completion triggered by <c-x><c-o>
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-    -- vim.keymap.set('n', '<C-.>', vim.lsp.buf.code_action, bufopts)
     -- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
     -- vim.keymap.set('n', '<leader>m', vim.lsp.buf.format, bufopts)
     -- vim.keymap.set('n', '<leader>lk', vim.lsp.buf.signature_help, bufopts)
@@ -21,6 +20,7 @@ local options = {
     -- vim.keymap.set('n', '<leader>ne', vim.diagnostic.goto_next)
     -- vim.keymap.set('n', '<leader>np', vim.diagnostic.goto_prev)
     -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', '<C-.>', vim.lsp.buf.code_action, bufopts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
     vim.keymap.set('n', 'gh', vim.lsp.buf.hover, bufopts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
@@ -29,6 +29,18 @@ local options = {
   end
 }
 
+-- NOTE: see all servers!
+-- @see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+
+-- NOTE: Server for JSON
+-- @see: https://github.com/hrsh7th/vscode-langservers-extracted
+lspconfig['jsonls'].setup({
+  capabilities = options.capabilities,
+  flags = options.flags,
+  on_attach = options.on_attach,
+})
+
+-- NOTE: Server for Lua
 -- @see: https://github.com/sumneko/lua-language-server
 lspconfig['sumneko_lua'].setup({
   capabilities = options.capabilities,
@@ -46,6 +58,7 @@ lspconfig['sumneko_lua'].setup({
   }
 })
 
+-- NOTE: Server for TypeScript
 -- @see: https://github.com/typescript-language-server/typescript-language-server
 lspconfig['tsserver'].setup({
   capabilities = options.capabilities,
