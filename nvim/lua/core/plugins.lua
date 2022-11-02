4eeb3b25319e5f18b5cace04af680f2d486dacb6
Alberto Silva <albertos@nvidia.com>
2022-11-02 11:08:48 -0400

    feat(workspace): basic completion

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -6,24 +6,34 @@ packer.startup(function(use)
   use('nathom/filetype.nvim')
   use('wbthomason/packer.nvim')
 
+  -- utility
   use({
     'nvim-lua/plenary.nvim',
     module = 'plenary'
   })
 
+  -- session
   use({
     'rmagatti/auto-session',
     config = require('plugins.auto-session').setup
   })
 
+  -- colorscheme
   use({
     'navarasu/onedark.nvim',
     config = require('plugins.onedark').setup
   })
 
+  -- language server
   use({
     'neovim/nvim-lspconfig',
     config = require('plugins.lspconfig').setup,
+  })
+
+  -- completion
+  use({
+    'L3MON4D3/LuaSnip',
+    module = 'luasnip'
   })
 
   use({
@@ -31,6 +41,10 @@ packer.startup(function(use)
     event = { 'User Defer' },
     config = require('plugins.completion').setup
   })
+
+  use({ after = 'nvim-cmp', 'hrsh7th/cmp-buffer' })
+  use({ after = 'nvim-cmp', 'hrsh7th/cmp-path' })
+  use({ after = 'nvim-cmp', 'saadparwaiz1/cmp_luasnip' })
 
   if (is_init) then
     packer.sync()
--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -1,6 +1,30 @@
 local M = {}
 
 M.setup = function()
+  local cmp = require('cmp')
+  local luasnip = require('luasnip')
+
+  cmp.setup({
+    snippet = {
+      expand = function(args)
+        luasnip.lsp_expand(args.body)
+      end,
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-n>'] = cmp.mapping.select_next_item(),
+      ['<C-p>'] = cmp.mapping.select_prev_item(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    }),
+    sources = cmp.config.sources({
+      { name = 'luasnip' },
+      { name = 'buffer' },
+      { name = 'path' },
+    })
+  })
 end
 
 return M
