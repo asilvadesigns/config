0070f80c7cac4f6b204b4b3e9a98a96b47ad1979
Alberto Silva <albertos@nvidia.com>
2022-11-02 17:08:03 -0400

    feat(workspace): cleanup

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -5,64 +5,39 @@ packer.startup(function(use)
   use('wbthomason/packer.nvim')
 
   -- utility
-  use({
-    'nvim-lua/plenary.nvim',
-    module = 'plenary'
-  })
+  use({ 'nvim-lua/plenary.nvim', module = 'plenary' })
 
   -- session
-  use({
-    'rmagatti/auto-session',
-    config = require('plugins.auto-session').setup
-  })
+  use({ 'rmagatti/auto-session', config = require('plugins.session').setup })
 
   -- colorscheme
-  use({
-    'navarasu/onedark.nvim',
-    config = require('plugins.onedark').setup
-  })
+  use({ 'navarasu/onedark.nvim', config = require('plugins.onedark').setup })
 
   -- language server
-  use({
-    'neovim/nvim-lspconfig',
-    module = 'lspconfig'
-  })
+  use({ 'neovim/nvim-lspconfig', module = 'lspconfig' })
 
-  use({
-    'williamboman/mason.nvim',
-    module = 'mason'
-  })
+  -- language server package manager
+  use({ 'williamboman/mason.nvim', module = 'mason' })
+  use({ 'williamboman/mason-lspconfig.nvim', module = 'mason-lspconfig' })
 
-  use({
-    'williamboman/mason-lspconfig.nvim',
-    module = 'mason-lspconfig'
-  })
-
-  use({
-    'onsails/lspkind.nvim',
-    module = 'lspkind'
-  })
-
-  -- completion
-  use({
-    'L3MON4D3/LuaSnip',
-    module = 'luasnip'
-  })
-
-  use({
-    'hrsh7th/cmp-nvim-lsp',
-    module = 'cmp_nvim_lsp'
-  })
-
-  use({ after = 'nvim-cmp', 'hrsh7th/cmp-buffer' })
-  use({ after = 'nvim-cmp', 'hrsh7th/cmp-path' })
-  use({ after = 'nvim-cmp', 'saadparwaiz1/cmp_luasnip' })
-
+  -- completion engine
   use({
     'hrsh7th/nvim-cmp',
     event = { 'User Defer' },
     config = require('plugins.completion').setup
   })
+
+  -- completion icons
+  use({ 'onsails/lspkind.nvim', module = 'lspkind' })
+
+  -- completion snippets
+  use({ 'L3MON4D3/LuaSnip', module = 'luasnip' })
+
+  -- completion sources
+  use({ 'hrsh7th/cmp-nvim-lsp', module = 'cmp_nvim_lsp' })
+  use({ 'hrsh7th/cmp-buffer', after = 'nvim-cmp' })
+  use({ 'hrsh7th/cmp-path', after = 'nvim-cmp'  })
+  use({ 'saadparwaiz1/cmp_luasnip', after = 'nvim-cmp'  })
 
   if (is_init) then
     packer.sync()
