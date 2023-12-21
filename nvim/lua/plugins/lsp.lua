33d2d947df99ba5fecec9205f09c607966985784
Alberto Silva <albertos@nvidia.com>
2023-12-21 09:24:20 -0500

    feat(workspace): fluffier

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,7 +1,7 @@
 return {
   {
     "hrsh7th/nvim-cmp",
-    event = { "VeryLazy" },
+    event = { "CursorMoved", "InsertEnter" },
     dependencies = {
       "L3MON4D3/LuaSnip",
       "hrsh7th/cmp-nvim-lsp",
