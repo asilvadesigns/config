cb94efcaf6cdd8e829de60f072b6212c3ff63aaa
Alberto Silva <asilvadesigns@gmail.com>
2024-04-16 11:30:04 -0400

    feat(workspace): package updates

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -28,6 +28,7 @@ return {
       vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
       vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
       vim.keymap.set("n", "gd", require("telescope.builtin").lsp_definitions, opts)
+      vim.keymap.set("n", "gi", require("telescope.builtin").lsp_implementations, opts)
       vim.keymap.set("n", "gl", function()
         require("telescope.builtin").lsp_definitions({ jump_type = "vsplit" })
       end, opts)
