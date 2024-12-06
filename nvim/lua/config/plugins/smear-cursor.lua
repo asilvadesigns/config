4c785e0eb73fec74ea6bb460efd4f06dafc694ff
Alberto Silva <asilvadesigns@gmail.com>
2024-12-05 21:47:11 -0500

    fix(elements): fluffier smear

--- nvim/lua/config/plugins/smear-cursor.lua
+++ nvim/lua/config/plugins/smear-cursor.lua
@@ -2,11 +2,22 @@ local M = {}
 
 M.setup = function()
   require("smear_cursor").setup({
+    -- cursor_color = "#e78284",
+    -- stiffness = 0.6,
+    -- trailing_stiffnewss = 0.1,
+    -- trailing_exponent = 5,
+    -- gamma = 1,
+    --
     cursor_color = "#e78284",
+    time_interval = 17,
+    max_slope_horizontal = 0.5,
+    min_slope_vertical = 2,
+    gamma = 2.2,
+    color_levels = 16,
+    distance_stop_animating = 0.1,
     stiffness = 0.6,
-    trailing_stiffnewss = 0.1,
-    trailing_exponent = 5,
-    gamma = 1,
+    trailing_stiffnewss = 0.3,
+    trailing_exponent = 0.1,
   })
 end
 
